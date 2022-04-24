addba7749c44d7b1ca2c7eecef611033d7a08fe7
altanbgn <altanbagana@erxes.io>
2022-04-24 19:41:41 +0800

    invite permission update

--- arctis-discord-bot/commands/invite.js
+++ arctis-discord-bot/commands/invite.js
@@ -9,7 +9,7 @@ module.exports = {
   execute(message) {
     return message.member
       .send(
-        `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot`
+        `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=8`
       )
       .catch(console.error);
   }
